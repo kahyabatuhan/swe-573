7ea4240da0a52b3fd4800b443f627aa73e1f0b8b
yelikmelik <yelikmelik@users.noreply.github.com>
2017-02-12 12:32:02 +0300

    database setup

--- my_django18_project/my_django18_project/settings.py
+++ my_django18_project/my_django18_project/settings.py
@@ -74,13 +74,20 @@ WSGI_APPLICATION = 'my_django18_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'localuser',
+        'PASSWORD': 'local007',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
