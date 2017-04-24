192b6a4755c16d61440c6c581973a4e7efaf33d1
kahyabatuhan <kahyabatuhan@gmail.com>
2017-04-25 01:22:05 +0300

    SignUpForm deleted, ContactForm created

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,8 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.shortcuts import render
 
+from .forms import ContactForm
 # Create your views here.
 def home(request):
 
@@ -8,10 +11,41 @@ def home(request):
 	return render(request, template, context)
 
 def contact(request):
+	title = 'Contact Us'
+	title_align_center = True
+	form = ContactForm(request.POST or None)
+	if form.is_valid():
+		# for key, value in form.cleaned_data.iteritems():
+		# 	print key, value
+		# 	#print form.cleaned_data.get(key)
+		form_email = form.cleaned_data.get("email")
+		form_message = form.cleaned_data.get("message")
+		form_full_name = form.cleaned_data.get("full_name")
+		# print email, message, full_name
+		subject = 'Site contact form'
+		from_email = settings.EMAIL_HOST_USER
+		to_email = [from_email, 'youotheremail@email.com']
+		contact_message = "%s: %s via %s"%( 
+				form_full_name, 
+				form_message, 
+				form_email)
+		some_html_message = """
+		<h1>hello</h1>
+		"""
+		send_mail(subject, 
+				contact_message, 
+				from_email, 
+				to_email, 
+				html_message=some_html_message,
+				fail_silently=True)
 
-	context = locals()
-	template = 'contact.html'
-	return render(request, template, context)
+	context = {
+		"form": form,
+		"title": title,
+		"title_align_center": title_align_center,
+	}
+	return render(request, "forms.html", context)
+
 
 def about(request):
 
