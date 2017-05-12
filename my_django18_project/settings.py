0795239e7f534411030b1d075cfe8a9daccb9578
kahyabatuhan <kahyabatuhan@gmail.com>
2017-05-12 18:58:22 +0300

    try req. modification4

--- my_django18_project/settings.py
+++ my_django18_project/settings.py
@@ -56,7 +56,8 @@ DEBUG = True
 
 import dj_database_url
 # Update database configuration with $DATABASE_URL.
-DATABASES['default'] =  dj_database_url.config()
+#DATABASES['default'] =  dj_database_url.config()
+DATABASES = { 'default': dj_database_url.config() }
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
