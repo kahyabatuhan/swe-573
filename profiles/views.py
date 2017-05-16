5f40b0bc549fda81e28dd776220ee20a344a8cf8
kahyabatuhan <kahyabatuhan@gmail.com>
2017-05-16 16:22:25 +0300

    back to production

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,8 @@
 from django.conf import settings
 from django.core.mail import send_mail
-
 from django.shortcuts import render
 
 from .forms import ContactForm
-
 
 # Create your views here.
 def home(request):
