75d0bc002ebfeccfa3c9e9d9d474696384b99dcc
kahyabatuhan <kahyabatuhan@gmail.com>
2017-12-07 12:15:31 +0300

    model tweet records created

---
+++ src/profiles/records.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.conf import settings
+
+class Record(models.Model):
+    tweet = models.CharField(max_length=255, unique=True)
+    score = models.IntegerField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    def save(self, *args, **kwargs):
+    	self.full_clean()
+    	super(Record, self).save(*args,**kwargs)
+
+    def delete(self, *args, **kwargs):
+    	self.full_clean()
+    	super(Record, self).delete(*args,**kwargs)
+
+
