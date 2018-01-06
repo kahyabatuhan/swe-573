f51c7d815cf481973c5f11b217db3a21bef519bd
kahyabatuhan <kahyabatuhan@gmail.com>
2018-01-06 21:59:48 +0300

    Update settings.py
    comment out SECURE_PROXY_SSL_HEADER

--- my_django18_project/settings.py
+++ my_django18_project/settings.py
@@ -167,7 +167,7 @@ if not DEBUG:
     DATABASES = { 'default': dj_database_url.config() }
     
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    #SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     
     # Allow all host headers
     ALLOWED_HOSTS = ['tossaword.com','www.tossaword.com']
