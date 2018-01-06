ef8875495cd1a5f892a38731ceeeb01720748341
kahyabatuhan <kahyabatuhan@gmail.com>
2018-01-06 21:52:57 +0300

    back to production
    set debug false

--- my_django18_project/settings.py
+++ my_django18_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3xzhjkgr@l!db8iuq%n8l)o8+dx6z-4mo3fb-lf7!(63mpm$ky'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #email credentials
 EMAIL_HOST = 'smtp.gmail.com'
